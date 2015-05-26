404d27fd286d99fb8ae8c3d07b22f75d93dfb1b9
Øistein Sørensen <sorenso@gmail.com>
2015-05-26 16:20:20 +0200

    Fiddeling with apache2 config.

--- modules/phpapp/manifests/postwork.pp
+++ modules/phpapp/manifests/postwork.pp
@@ -2,12 +2,21 @@ class phpapp::postwork {
 
   file { "apache2_apache2_conf":
     name => "/etc/apache2/apache2.conf",
-    notify  => Service["apache2"],  # this sets up the relationship
     ensure => file,
     owner => root,
     group => root,
     mode  => 644,
     source => "/srv/config/${::apache_config}/apache2.conf",
+    require => [Class["phpapp::prework"], Class["phpapp::core"]],
+  } ->
+
+  file { "apache2_ports_conf":
+    name => "/etc/apache2/ports.conf",
+    ensure => file,
+    owner => root,
+    group => root,
+    mode  => 644,
+    source => "/srv/config/${::apache_config}/ports.conf",
     require => [Class["phpapp::prework"], Class["phpapp::core"]],
   }
 
@@ -15,7 +24,7 @@ class phpapp::postwork {
     name => "/etc/apache2/httpd.conf",
     ensure => present,
     content => "",
-  }
+  } ->
 
   file { "apache2_sites_enabled_1":
     name => "/etc/apache2/sites-enabled/${::php_domain_1}",
