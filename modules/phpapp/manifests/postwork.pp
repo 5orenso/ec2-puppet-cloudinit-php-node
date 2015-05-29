dd3b28a6cc4dc07cebc30b4569ed171897a2befb
Øistein Sørensen <sorenso@gmail.com>
2015-05-29 14:13:45 +0200

    Fixed order of execution.

--- modules/phpapp/manifests/postwork.pp
+++ modules/phpapp/manifests/postwork.pp
@@ -39,16 +39,6 @@ class phpapp::postwork {
     source => "/srv/config/${::apache_config}/sites-enabled/${::php_domain_1}",
     require => [Class["phpapp::prework"], Class["phpapp::core"]],
   } ->
-  file { "apache2_apache2_conf":
-    name => "/etc/apache2/apache2.conf",
-    ensure => file,
-    owner => root,
-    group => root,
-    mode  => 644,
-    source => "/srv/config/${::apache_config}/apache2.conf",
-    require => [Class["phpapp::prework"], Class["phpapp::core"]],
-  } ->
-
   file { "apache2_ports_conf":
     name => "/etc/apache2/ports.conf",
     ensure => file,
@@ -58,14 +48,20 @@ class phpapp::postwork {
     source => "/srv/config/${::apache_config}/ports.conf",
     require => [Class["phpapp::prework"], Class["phpapp::core"]],
   } ->
-
   file { "apache2_httpd_conf":
     name => "/etc/apache2/httpd.conf",
     ensure => present,
     content => "",
   } ->
-
-
+  file { "apache2_apache2_conf":
+    name => "/etc/apache2/apache2.conf",
+    ensure => file,
+    owner => root,
+    group => root,
+    mode  => 644,
+    source => "/srv/config/${::apache_config}/apache2.conf",
+    require => [Class["phpapp::prework"], Class["phpapp::core"]],
+  } ->
 
   file { "varnish_config":
     name => "/etc/default/varnish",
