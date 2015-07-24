1c7f7289aa983c825d80bd4aa87488670fcb0efa
Øistein Sørensen <sorenso@gmail.com>
2015-07-24 10:42:23 +0200

    Working versions

--- remove_all_packages.sh
+++ remove_all_packages.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
 
 # Remove all files.
-sudo service simple-blog stop
+sudo service Zu-CMS stop
+sudo service varnish stop
+sudo service apache2 stop
+
+sudo rm -rf /srv/
+sudo mv /etc/puppet.orig/ /etc/puppet/
+
+sudo rm -f /etc/init/Zu-CMS.conf
+sudo rm -f /etc/logrotate.d/Zu-CMS
+sudo rm -f /var/log/upstart/Zu-CMS.log
+
+sudo rm -f /etc/apache2/
+sudo rm -f /etc/varnish/
+sudo rm -f /etc/default/apache2
+sudo rm -f /etc/default/varnish*
+
+sudo rm -rf /var/log/Zu-CMS/
+sudo rm -rf /var/run/Zu-CMS/
+
+sudo rm -f /etc/init/FFE-CMS.conf
+sudo rm -f /etc/logrotate.d/FFE-CMS
+sudo rm -f /var/log/upstart/FFE-CMS.log
+
+sudo rm -rf /var/log/FFE-CMS/
+sudo rm -rf /var/run/FFE-CMS/
+
+sudo rm -rf /var/www/lib/
+sudo rm -rf /var/www/dev.zu.no/
+sudo rm -rf /var/www/dealer.flyfisheurope.com/
+sudo rm -rf /var/www/www.flyfisheurope.com/
+
+pear uninstall Mail
+pecl uninstall mongo
+
 
 sudo apt-get remove --purge nodejs
 sudo apt-get remove --purge unzip
@@ -16,22 +49,8 @@ sudo apt-get remove --purge php5-gd
 sudo apt-get remove --purge varnish
 sudo apt-get remove --purge apache2
 
-sudo rm -rf /srv/
-sudo mv /etc/puppet.orig/ /etc/puppet/
-
-sudo rm -f /etc/init/simple-blog.conf
-sudo rm -f /etc/logrotate.d/simple-blog
-sudo rm -f /var/log/upstart/simple-blog.log
-
-sudo rm -rf /var/log/simple-blog/
-sudo rm -rf /var/run/simple-blog/
-
-sudo rm -rf /var/www/dev.zu.no/
-sudo rm -rf /var/www/dealer.flyfisheurope.com/
-sudo rm -rf /var/www/www.flyfisheurope.com/
-
 # Check all files.
-sudo ps aux | grep node && sudo ls -al /srv/ /etc/init/simple-blog.conf /etc/logrotate.d/simple-blog /var/log/simple-blog/ /etc/puppet/ /var/run/simple-blog/
+sudo ps aux | grep node && sudo ls -al /srv/ /etc/init/Zu-CMS.conf /etc/logrotate.d/Zu-CMS /var/log/Zu-CMS/ /etc/puppet/ /var/run/Zu-CMS/
 
 # Run user_data.sh
 # sudo bash ~/user_data.sh
--- user-data-dist.sh
+++ user-data-dist.sh
@@ -10,9 +10,26 @@ mv /etc/puppet /etc/puppet.orig
 git clone https://github.com/5orenso/ec2-puppet-cloudinit-php-node.git /etc/puppet
 
 # Run puppet.
-APP="simple-blog"
+NODEAPP="Zu-CMS"
+PHPAPP="FFE-CMS"
+
 export FACTER_gitconfigrepo="https://<username>:<password>@github.com/<username>/<config repo>.git"
-export FACTER_gitrepo="https://github.com/<username>/$APP.git"
-export FACTER_appname="$APP"
-export FACTER_appconfig="$APP/litt.no"
+
+export FACTER_node_gitrepo="https://github.com/<username>/$NODEAPP.git"
+export FACTER_node_appname="$NODEAPP"
+export FACTER_node_appconfig="ffe/$NODEAPP/zu"
+
+export FACTER_php_gitrepo="https://<username>:<password>@github.com/<username>/$PHPAPP.git"
+export FACTER_php_appname="$PHPAPP"
+export FACTER_php_appconfig="ffe/$PHPAPP"
+
+export FACTER_php_domain_1="dev.zu.no"
+export FACTER_php_domain_2="dealer.flyfisheurope.com"
+export FACTER_php_domain_3="www.flyfisheurope.com"
+
+export FACTER_apache_config="ffe/apache2"
+export FACTER_varnish_config="ffe/varnish"
+export FACTER_php_config="ffe/php5"
+export FACTER_etc_config="ffe/etc"
+
 puppet apply /etc/puppet/manifests/mainrun.pp
