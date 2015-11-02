5c048ee39ee827b327e7c29338f22466ed107519
Øistein Sørensen <sorenso@gmail.com>
2015-11-02 09:04:08 +0100

    Moved to phpapp-core.

--- modules/nodeapp/manifests/core.pp
+++ modules/nodeapp/manifests/core.pp
@@ -1,20 +1,20 @@
 class nodeapp::core {
     notice("I am a node ${::node_appname} and want the software")
 
-    exec { "nodeapp_download_config":
-        command => "git clone ${::gitconfigrepo} /srv/config/",
-        path    => "/usr/local/bin/:/usr/bin/:/bin/",
-        cwd     => "/srv/",
-        require => Class["nodeapp::prework"]
-    } ->
-
-    exec { 'nodeapp_www_data_chown':
-        command  => "/bin/chown -R www-data:www-data /srv/"
-    } ->
-
-    exec { 'nodeapp_www_data_chmod':
-        command  => "/bin/chmod -R 755 /srv/"
-    } ->
+#    exec { "nodeapp_download_config":
+#        command => "git clone ${::gitconfigrepo} /srv/config/",
+#        path    => "/usr/local/bin/:/usr/bin/:/bin/",
+#        cwd     => "/srv/",
+#        require => Class["nodeapp::prework"]
+#    } ->
+#
+#    exec { 'nodeapp_www_data_chown':
+#        command  => "/bin/chown -R www-data:www-data /srv/"
+#    } ->
+#
+#    exec { 'nodeapp_www_data_chmod':
+#        command  => "/bin/chmod -R 755 /srv/"
+#    } ->
 
     exec { "nodeapp_download_src":
         command => "git clone ${::node_gitrepo} /srv/${::node_appname}/",
@@ -35,7 +35,7 @@ class nodeapp::core {
         name    => "/srv/${::node_appname}/config/config.js",
         ensure  => link,
         target  => "/srv/config/${::node_appconfig}/config.js",
-        require => Class["nodeapp::prework"]
+        require => Class["nodeapp::prework", "phpapp::core"]
     } ->
 
     file { "nodeapp_logs" :
