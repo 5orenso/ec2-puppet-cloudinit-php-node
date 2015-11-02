524e298d087a864d5dc6478a3e245a338938c0ac
Øistein Sørensen <sorenso@gmail.com>
2015-11-02 09:39:40 +0100

    Added apache2 php5 module.

--- modules/base/manifests/apache2.pp
+++ modules/base/manifests/apache2.pp
@@ -4,27 +4,30 @@ class base::apache2 {
   package { 'apache2' :
     ensure => installed,
   }
+  package { 'libapache2-mod-php5' :
+    ensure => installed,
+  }
   exec { 'enable_module_headers' :
     command => 'a2enmod headers',
     path    => '/usr/local/bin/:/usr/bin/:/bin/:/usr/sbin/',
-    require => Package['apache2']
+    require => Package['apache2', 'libapache2-mod-php5']
   }
   exec { 'enable_module_expires' :
     command => 'a2enmod expires',
     path    => '/usr/local/bin/:/usr/bin/:/bin/:/usr/sbin/',
-    require => Package['apache2']
+    require => Package['apache2', 'libapache2-mod-php5']
   }
   exec { 'enable_module_rewrite' :
     command => 'a2enmod rewrite',
     path    => '/usr/local/bin/:/usr/bin/:/bin/:/usr/sbin/',
-    require => Package['apache2']
+    require => Package['apache2', 'libapache2-mod-php5']
   }
 
   # define the service to restart
   service { "apache2":
     ensure  => "running",
     enable  => "true",
-    require => Package["apache2"],
+    require => Package["apache2", 'libapache2-mod-php5'],
     subscribe => File["/etc/apache2/apache2.conf"],
   }
 
